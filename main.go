ca045d09a33389c9af9ed1ad9b5abb3f8241ba3f
Boris Lie <Boris.YH.Lie@pccw.com>
2022-06-17 16:13:41 +0800

    Update check direction

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func handler(w http.ResponseWriter, req *http.Request) {
 }
 
 func play(input ArenaUpdate) (response string) {
-	log.Printf("IN: %#v", input)
+	//log.Printf("IN: %#v", input)
 	//test
 	doubleMay := input.Links.Self.Href
 	doubleMayObj := input.Arena.State[doubleMay]
@@ -53,7 +53,25 @@ func play(input ArenaUpdate) (response string) {
 	doubleMayX := doubleMayObj.X
 	doubleMayY := doubleMayObj.Y
 	log.Printf("DoubleMay URL:" + doubleMay + " areaXmax:" + strconv.Itoa(areaXmax) + " doubleMayX:" + strconv.Itoa(doubleMayX) + " areaYmax:" + strconv.Itoa(areaYmax) + " doubleMayY:" + strconv.Itoa(doubleMayY) + " doubleMayDirection:" + doubleMayDirection)
-
+	//Check chikan location
+	for key, chikan := range input.Arena.State {
+		log.Println("Key:", key, "=>", "Element:", chikan)
+		if key == doubleMay {
+			continue
+		}
+		if doubleMayDirection == "N" {
+			log.Printf("Hit N")
+		}
+		if doubleMayDirection == "E" {
+			log.Printf("Hit E")
+		}
+		if doubleMayDirection == "S" {
+			log.Printf("Hit S")
+		}
+		if doubleMayDirection == "W" {
+			log.Printf("Hit W")
+		}
+	}
 	commands := []string{"F", "R", "L", "T"}
 	rand := rand2.Intn(4)
 	return commands[rand]
